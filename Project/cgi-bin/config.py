f51b20f8d7d8fe24becfcefa83f0e6a649477df5
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2019-05-26 14:20:31 +0100

    removed unnecessary comments

--- Project/cgi-bin/config.py
+++ Project/cgi-bin/config.py
@@ -1,9 +1,4 @@
-"""
-	Define global config variables here.
-	These will be accessible from other
-	scripts, so we only need to set them
-	here.
-"""
+
 
 # app configurables stored in a config object
 config = {
